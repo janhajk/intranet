0abc81a5ef3685d483d03e55289b72390107b230
janhajk <janhajk@gmail.com>
2014-12-16 11:41:27 +0000

    missing file

---
+++ pumpkin/parts/footer.php
@@ -0,0 +1,3 @@
+<div class="box" style="text-align:center;font-size:7pt;">
+Pumpkin-Framework is &copy; 2009 by <?=end(getVar('pmkPageProducer'));?> (<a href="mailto:jan.schaer@janschaer.ch">Jan Schär</a>)
+</div>
