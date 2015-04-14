ffe919ea08d53e2547c834bd880177298841e9f7
janhajk <janhajk@gmail.com>
2015-04-14 13:03:19 +0000

    add bootstrap

--- pumpkin/sites/mobile/pages/p.arbeit.php
+++ pumpkin/sites/mobile/pages/p.arbeit.php
@@ -19,7 +19,7 @@
             <div>
                 <ul class="list-group">
                     <?php
-                    $items = stunden_overview();
+                    $items = stunden_overview(5);
                     foreach ($items as $key=>$value) {
                         ?><li><?=substr($value['date'],5,5); echo substr($value['von'],0,5);echo substr($value['bis'],0,5);?><a href="?p=arbeit.stunden.edit&id=<?=$value['id'];?>"><?=htmlspecialchars(substr($value['comment'],0,23));?></a></li><?php
                     }
