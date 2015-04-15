c397c04abccef0753382b78efcfa259488cda88b
janhajk <janhajk@gmail.com>
2015-04-15 10:44:15 +0000

    add bootstrap to arbeit

--- pumpkin/sites/mobile/pages/p.arbeit.stunden.php
+++ pumpkin/sites/mobile/pages/p.arbeit.stunden.php
@@ -3,12 +3,26 @@
 	if(isset($_GET['l'])) {$limit = (int) $_GET['l'];}
 	?>
     <div class="container" style="max-width:700px !important;">
-        <a href="index.php?p="<?php echo $pages['Arbeit'][0]; ?> role="button" class="btn btn-default btn-sm btn-block">back</a>
+        <a href="index.php?p=<?php echo $pages['Arbeit'][0]; ?>" role="button" class="btn btn-default btn-sm btn-block">back</a>
         <h2>Time Sheet</h2>
         <a href="<?=SITE_HTML;?>/actions.php?a=xlsstunden">download</a>
-        <div>
-            <h3>Last <?php echo $limit; ?></h3>
-            <table><? stunden_overview($limit); ?></table>
+
+        <h3>Last <?php echo $limit; ?></h3>
+        <div class="list-group">
+            <?php
+            $items = stunden_overview($limit);
+            foreach ($items as $key=>$value) {?>
+            <a href="?p=arbeit.stunden.edit&id=<?php echo $value['id'];?>" class="list-group-item">
+                <h4 class="list-group-item-heading"><?php echo $value['date'] . ' '.substr($value['von'],0,5).'-'.substr($value['bis'],0,5);?></h4>
+                <p class="list-group-item-text">
+                    <?php echo htmlspecialchars($value['comment']);?>
+                </p>
+            </a><?php
+                                     }
+            ?>
+            <a href="?p=arbeit.stunden&l=10" class="list-group-item">
+                see all
+            </a>
         </div>
         <a href="<?=str_replace("l=10", "l=30", $_SERVER["REQUEST_URI"]);?>">more</a>
     </div>
