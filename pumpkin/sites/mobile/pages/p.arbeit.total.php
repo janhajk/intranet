398d0d04230e978826bedb96ed0c11d990d58de3
janhajk <janhajk@gmail.com>
2015-04-15 19:39:19 +0000

    add bootstrap to arbeit

--- pumpkin/sites/mobile/pages/p.arbeit.total.php
+++ pumpkin/sites/mobile/pages/p.arbeit.total.php
@@ -19,7 +19,7 @@
         <h2>Totals</h2>
         <div>
             <h3>per contract</h3>
-            <table class="table" align="right">
+            <table class="table">
                 <thead>
                     <tr>
                         <th>Contract</th>
@@ -47,14 +47,13 @@ function getTotalVertrag() {
 	$db->query($sql);
 	while($i = $db->results()) { if($i['title']!='')$totals[] = $i; }
 	foreach($totals as $t) {
-		echo "<tr><td><a href=\"".SITE_HTML."/actions.php?a=xlsstunden&vertrag=".$t['ID']."\">".$t['title']."</a></td><td>".round($t['t'])."h</td><td>".round($t['nettoansatz']).".-</td><td>".number_format(round($t['c']),0,'.','`').".-</td></tr>";
+		echo "<tr><td><a href=\"".SITE_HTML."/actions.php?a=xlsstunden&vertrag=".$t['ID']."\">".$t['title']."</a></td><td>".round($t['t'])."h</td><td>".round($t['nettoansatz']).".-</td><td align="right">".number_format(round($t['c']),0,'.','`').".-</td></tr>";
 	}
 }
 
 function getTotalJahr($year) {
 	$total = 0;
 	$db = $GLOBALS['db'];
-	$db->query("SET NAMES 'utf8';");  // Damit die Umlaute richtig dargestellt werden
 	$sql = "SELECT rap_vertrag.title, rap_vertrag.nettoansatz*SUM(rap_stunden.stunden) as c, SUM(rap_stunden.stunden) as t FROM rap_stunden LEFT JOIN rap_vertrag ON (rap_stunden.vertrag=rap_vertrag.ID) WHERE rap_stunden.date >= '$year-01-01' AND rap_stunden.date < '".($year+1)."-01-01' GROUP BY rap_stunden.vertrag";
 	$db->query($sql);
 	while($i = $db->results()) { if($i['title']!='')$totals[] = $i; }
