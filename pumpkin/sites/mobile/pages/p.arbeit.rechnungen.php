9633775bb02aa74dd204927caf558efbab336378
janhajk <janhajk@gmail.com>
2015-04-15 18:58:20 +0000

    add bootstrap to arbeit

--- pumpkin/sites/mobile/pages/p.arbeit.rechnungen.php
+++ pumpkin/sites/mobile/pages/p.arbeit.rechnungen.php
@@ -1,7 +1,20 @@
 <? // Arbeit.Rechnungen Seite ?>
 <? if(isset($_GET[$page]) && $_GET[$page] == 'arbeit.rechnungen' && canEnterBinary('0.0.0.0.0.0.0.0.0.1')) {	?>
     <div class="container" style="max-width:700px !important;">
-        <a href="index.php?p=<?php echo $pages['Arbeit'][0]; ?>" role="button" class="btn btn-default btn-sm btn-block">back</a>
+        <ul class="nav nav-pills" role="tablist">
+            <li><a href="index.php"><span class="glyphicon glyphicon-home"></span></a></li>
+            <li class="active"><a href="?p=arbeit.rechnungen">Invoices</a></li>
+            <li class="dropdown">
+                <a class="dropdown-toggle" data-toggle="dropdown" href="#">Time Reports <span class="caret"></span></a>
+                <ul class="dropdown-menu" role="menu">
+                    <li><a href="<?=SITE_HTML;?>/actions.php?a=xlsstunden&vertrag=0">xls</a></li>
+                    <li><a href="?p=arbeit.timeline&timeline=1">Timeline</a></li>
+                    <li><a href="?p=arbeit.graphs">Graphs</a></li>
+                </ul>
+            </li>
+            <li><a href="?p=arbeit.total">Contracts</a></li>
+            <li><a href="?p=arbeit.admin"><span class="glyphicon glyphicon-wrench"></span></a></li>
+        </ul>
         <h2>Rechnungen</h2>
         <form class="form-inline" role="form" action="<?=SITE_HTML;?>/actions.php" method="post">
             <div class="form-group">
