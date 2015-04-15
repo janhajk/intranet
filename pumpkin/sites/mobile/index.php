874b38c0d1ea8ec315ea6dc7def3aab4d0adbc7a
janhajk <janhajk@gmail.com>
2015-04-15 14:13:11 +0000

    add bootstrap to arbeit

--- pumpkin/sites/mobile/index.php
+++ pumpkin/sites/mobile/index.php
@@ -15,6 +15,8 @@
 
         <title>intranet</title>
 
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+
         <!-- Bootstrap -->
         <!-- Latest compiled and minified CSS -->
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
