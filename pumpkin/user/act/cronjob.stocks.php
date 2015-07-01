bc7ede2ad0323fb23c226573a8825c1eab583023
janhajk <janhajk@gmail.com>
2015-07-01 11:26:19 +0000

    redirect after cron stocks #2

--- pumpkin/user/act/cronjob.stocks.php
+++ pumpkin/user/act/cronjob.stocks.php
@@ -1,11 +1,12 @@
-<?
+<?php
 // **************************************************************************************************
 	//Lib laden
 	include($_SERVER['DOCUMENT_ROOT'].'/config.php');
 // **************************************************************************************************
 	//updateStatsStocks();
 	stocks_update();
-    echo '<script>(function(){window.setTimeout(function(){window.location.href = "index.php?p=1";}, 5000);}})();</script>';
+    // Redirect back
+    echo '<script>(function(){window.setTimeout(function(){window.location.href="/index.php?p=1";},5000);})();</script>';
 // **************************************************************************************************
 	//Lib schliessen
 	endlib();			// from lib/lib.php
