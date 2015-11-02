7d59d81c106c543d2cfe9c87195e233b254c46ae
janhajk <janhajk@gmail.com>
2015-11-02 10:33:10 +0000

    currency format apostroph

--- pumpkin/core/lib/functions.types.php
+++ pumpkin/core/lib/functions.types.php
@@ -1,35 +1,33 @@
 <?php
-	/*
-	Formatiert eine Währungszahl und fügt ein Plus voran, wenn positiv und gewünscht
+/*
+	Formatiert eine WÃ¤hrungszahl und fÃ¼gt ein Plus voran, wenn positiv und gewï¿½nscht
+   */
+function pmkCurrency($number, $precidingPlus=false) {
+   // Variable fÃ¼r Ausgabe
+   $n = '';
+
+   // Formatierung mit Tausenderstriche und Rundung auf Null Kommastellen
+   $n = number_format($number,0,",","'").'.-';
+
+   // Vorangehendes "+", wenn positiver Betrag und $precidingPlus = true
+   if($precidingPlus){
+      $n = ($number > 0) ? '+'.$n : $n;
+   }
+
+   return $n;
+}
+
+
+/*
+		Schneidet ein String nach $max Zeichen ab und fï¿½gt "..." an
 	*/
-	function pmkCurrency($number, $precidingPlus=false) {
-		// Variable für Ausgabe
-		$n = '';
-		
-		// Formatierung mit Tausenderstriche und Rundung auf Null Kommastellen
-		$n = number_format($number,0,",","`").'.-';
-		
-		// Vorangehendes "+", wenn positiver Betrag und $precidingPlus = true
-		if($precidingPlus){
-			$n = ($number > 0) ? '+'.$n : $n;
-		}
-		
-		// Rückgabe
-		return $n;
-	}
-	
-	
-	/*
-		Schneidet ein String nach $max Zeichen ab und fügt "..." an
-	*/
-	function pmkCutString($string, $max=55) {
-		// Variable für Ausgabe
-		$n = '';
-		
-		// Wenn String länger als $max, dann wird er abgeschnitten und "..." angehängt
-		$n = (strlen($string)>$max) ? substr($string,0,$max-3).'...':$string;
-		
-		// Rückgabe
-		return $n;
-	}
+function pmkCutString($string, $max=55) {
+   // Variable fï¿½r Ausgabe
+   $n = '';
+
+   // Wenn String lÃ¤nger als $max, dann wird er abgeschnitten und "..." angehÃ¤ngt
+   $n = (strlen($string)>$max) ? substr($string,0,$max-3).'...':$string;
+
+   return $n;
+}
 ?>
