866599352acab9794fc58ae7a76a1004b22a1cda
Jan <janhajk@gmail.com>
2018-07-06 20:11:03 +0000

    error übersicht

--- pumpkin/user/lib/functions.paper.php
+++ pumpkin/user/lib/functions.paper.php
@@ -11,14 +11,14 @@ function printSheet($von, $bis, $fach, $spezial, $matrix=1) {
 					$pdf->SetY(2);
 					$pdf->SetFont('Arial','B',20);
 					$pdf->Cell(0,10,$fach,0,0,'L');
-							
+
 			$pdf->SetFont('Arial','',10); 
 				// Begin: Header
 					$pdf->SetY(2);
 					$pdf->SetFont('Arial','B',7);
 					$pdf->Cell(0,10,'EDC_2010-xx-xx_'.$_SESSION['vorname'].'_FHNW_'.$fach.'_'.$spezial.'_p'.substr('000'.$i,strlen($i)),0,0,'R');
 				// Ende: Header
-			// erstellt die Häuschen für das Papier
+			// erstellt die Hï¿½uschen fï¿½r das Papier
 			$pdf->SetY(0);
 			//$pdf->SetX(10);
 			$pdf->SetX(0);
